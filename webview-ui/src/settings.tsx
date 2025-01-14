321f26dd8c023d8b616028e7e6ca928fb8a05eb4
luunderstars <corleonelou@qq.com>
2025-01-14 19:14:51 +0800

    fix chunkFileNames: `assets/

--- webview-ui/src/settings.tsx
+++ webview-ui/src/settings.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import Settings from './pages/Settings';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Settings />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 ); 
